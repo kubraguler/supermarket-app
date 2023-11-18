185559cea3d45a2692af277c3cf05712ce905be5
kubraguler <oktnkbr@gmail.com>
2023-11-18 23:40:08 +0000

    Create components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Header from "./components/Header/Header";
+import ProductList from "./components/ProductList/ProductList";
 import "./App.scss";
 
 function App() {
-	return <div className="App">Hello</div>;
+	return (
+		<div className="App">
+			<Header />
+			<ProductList />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/ProductList/ProductList.js
@@ -0,0 +1,5 @@
+const ProductList = () => {
+	return <div>Product List Page</div>;
+};
+
+export default ProductList;
