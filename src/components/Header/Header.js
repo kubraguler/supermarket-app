bf6194c1679869f9788edbbc7cb52baed578b005
kubraguler <oktnkbr@gmail.com>
2023-11-19 16:28:20 +0000

    Edit Header comp styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
-	return <nav>Header</nav>;
+	return (
+		<nav className="navigation">
+			<div className="logo">Logo</div>
+			<ul className="menu-list">
+				<li className="menu-list_item">
+					<Link href="/">Home</Link>
+				</li>
+				<li className="menu-list_item">
+					<Link href="/shop">Shop</Link>
+				</li>
+				<li className="menu-list_item">
+					<Link href="/cart">Cart</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Header;
