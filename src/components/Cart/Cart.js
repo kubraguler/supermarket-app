6400dd79152f92c5932014636538c8e62a53fb12
kubraguler <oktnkbr@gmail.com>
2023-11-23 16:38:37 +0000

    Show added products in the Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,25 @@
+import { useSelector } from "react-redux";
+import CartItem from "./CartItem";
 import "./Cart.scss";
 
 const Cart = () => {
-	return <div>Cart</div>;
+	const { cart } = useSelector((state) => state.cart);
+
+	return (
+		<div className="cart">
+			<div>
+				<h3>Shopping Cart</h3>
+				<ul>
+					{cart?.map((item) => (
+						<li key={item.id}>
+							<CartItem key={item.id} id={item.id} name={item.name} price={item.price} quantity={item.quantity} />
+							<button>Remove</button>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Cart;
